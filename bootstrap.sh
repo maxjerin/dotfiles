d03531e222aa3f78829adca229bef4fedf549195
Jerin Mathew <maxjerin@gmail.com>
2022-04-05 19:23:00 -0400

    streamline boostrap for macosu

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+
+# TODO
+# Check if 
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# source zshrc for homebrew
+source ~/.zshrc
+
+# install ansible using homebrew
+brew install ansible
+
 # Dotfiles' project root directory
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Host file location
@@ -13,6 +25,9 @@ PLAYBOOK="$ROOTDIR/dotfiles.yml"
 # apt-get update && apt-get install -y ansible
 
 # Runs Ansible playbook using our user.
-ansible-playbook -i "$HOSTS" "$PLAYBOOK"
+ansible-playbook -i "$HOSTS" "$PLAYBOOK" --tags "macos"
+
+# Link zshrc from dotfiles
+ln -s $(pwd)/.zshrc ~/.zshrc
 
 exit 0
