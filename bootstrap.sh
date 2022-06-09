784d1ea9e078691d475b577f213e01932ea99c29
Jerin Mathew <maxjerin@gmail.com>
2022-06-09 12:43:48 -0400

    streamline bootstrap script for linux and mac

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,33 +2,49 @@
 
 set -euox pipefail
 
-# Install Homebrew/Linuxbrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 # Link zshrc from dotfiles
-ln -s $(pwd)/.zsh_abbr ~/.zsh_abbr
-ln -s $(pwd)/.zshrc_base ~/.zshrc_base
+ln -sf $(pwd)/.zsh_abbr ~/.zsh_abbr
+ln -sf $(pwd)/.zshrc_base ~/.zshrc_base
 
 # Zsh and brew setup on MacOS
 if [[ $(uname -s) == 'Darwin' ]]; then
-    ln -s $(pwd)/.zshrc_macos ~/.zshrc
+    ln -fs $(pwd)/.zshrc_macos ~/.zshrc
 
-    # install ansible using homebrew
-    brew install ansible
+    if ! command -v brew &> /dev/null
+    then
+        # Install Homebrew/Linuxbrew
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+
+    if ! command -v ansible &> /dev/null
+    then
+        # install ansible using homebrew
+        brew install ansible
+    fi
 else
 # Zsh and brew setup on Linux
-    ln -s $(pwd)/.zshrc_linux ~/.zshrc
+    ln -sf $(pwd)/.zshrc_linux ~/.zshrc
 
-    # Homebrew build essentials
-    sudo apt update
-    sudo apt-get install -y build-essential
+    if ! command -v brew &> /dev/null
+    then
+        # Install Homebrew/Linuxbrew
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    # Installs ansible
-    sudo apt update
-    sudo apt install -y software-properties-common
-    sudo add-apt-repository --yes --update ppa:ansible/ansible
-    sudo apt update
-    sudo apt install -y ansible
+        # Homebrew build essentials
+        sudo apt update
+        sudo apt-get install -y build-essential
+
+    fi
+
+    if ! command -v ansible &> /dev/null
+    then
+        # Installs ansible
+        sudo apt update
+        sudo apt install -y software-properties-common
+        sudo add-apt-repository --yes --update ppa:ansible/ansible
+        sudo apt update
+        sudo apt install -y ansible
+    fi
 fi
 
 # source zshrc for homebrew
@@ -43,5 +59,16 @@ PLAYBOOK="$ROOTDIR/dotfiles.yml"
 
 # Runs Ansible playbook using our user.
 ansible-playbook -i "$HOSTS" "$PLAYBOOK"
+
+if [[ $(uname -s) == 'Linux' ]]; then
+    if command -v brew &> /dev/null
+    then
+        command -v zsh | sudo tee -a /etc/shells
+        chsh -s $(brew --prefix)/bin/zsh
+    else
+        echo "zsh not installed"
+    fi
+fi
+
 
 exit 0
