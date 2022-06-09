5863ba2324b31ad0b0cfb3b79d82805ca7290886
Jerin Mathew <maxjerin@gmail.com>
2022-06-09 00:27:52 -0400

    customize bootstrap for linux and macos

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,38 @@
 #!/usr/bin/env bash
 
-set -e
+set -euox pipefail
 
-# Zsh setup on MacOS
+# Install Homebrew/Linuxbrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Link zshrc from dotfiles
+ln -s $(pwd)/.zsh_abbr ~/.zsh_abbr
+ln -s $(pwd)/.zshrc_base ~/.zshrc_base
+
+# Zsh and brew setup on MacOS
 if [[ $(uname -s) == 'Darwin' ]]; then
-
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    # source zshrc for homebrew
-    source ~/.zshrc
+    ln -s $(pwd)/.zshrc_macos ~/.zshrc
 
     # install ansible using homebrew
     brew install ansible
-
-    # Link zshrc from dotfiles
-    ln -s $(pwd)/.zshrc ~/.zshrc
-    ln -s $(pwd)/.zsh_abbr ~/.zsh_abbr
 else
+# Zsh and brew setup on Linux
+    ln -s $(pwd)/.zshrc_linux ~/.zshrc
+
+    # Homebrew build essentials
+    sudo apt update
+    sudo apt-get install -y build-essential
+
     # Installs ansible
-    sudo apt update \
-    sudo apt install -y software-properties-common \
-    sudo add-apt-repository --yes --update ppa:ansible/ansible \
-    sudo apt update \
+    sudo apt update
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    sudo apt update
     sudo apt install -y ansible
 fi
+
+# source zshrc for homebrew
+source ~/.zshrc
 
 # Dotfiles' project root directory
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -33,6 +42,6 @@ HOSTS="$ROOTDIR/hosts"
 PLAYBOOK="$ROOTDIR/dotfiles.yml"
 
 # Runs Ansible playbook using our user.
-ansible-playbook -i "$HOSTS" "$PLAYBOOK" --tags "macos"
+ansible-playbook -i "$HOSTS" "$PLAYBOOK"
 
 exit 0
